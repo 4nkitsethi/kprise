c109b20cadc169909197bf9434ab10b821b13c58
Ankit Sethi <61274953+4nkitsethi@users.noreply.github.com>
2026-04-08 00:17:47 +0530

    ignore htaccess

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,15 +20,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
-        // This forces Laravel to use your .env APP_URL for all generated links
-        if (!empty(config('app.url'))) {
-            URL::forceRootUrl(config('app.url'));
-        }
-
-        // This ensures HTTPS is used for all links (fixing the broken CSS/JS)
-        if (app()->environment('production') || str_contains(config('app.url'), 'https')) {
-            URL::forceScheme('https');
-        }
     }
 }
